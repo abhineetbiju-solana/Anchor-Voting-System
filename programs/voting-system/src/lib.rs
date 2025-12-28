866ce1b07f919a52662e697555fe98044caabffc
Abhineet Biju <abhineet.biju19@gmail.com>
2025-12-28 20:22:48 +0530

    1. Designed structs for the program. 2. Added Cargo.lock file to stage.

--- programs/voting-system/src/lib.rs
+++ programs/voting-system/src/lib.rs
@@ -12,5 +12,34 @@ pub mod voting_system {
     }
 }
 
+//designing struct for creating the poll
+#[account]
+#[derive(InitSpace)]
+pub struct Poll {
+    pub poll_id: u64,
+    pub authority: Pubkey,
+    #[max_len(256)]
+    pub description: String,
+    #[max_len(10)]
+    pub options: Vec<Options>,
+    pub status: bool,
+}
+//designing struct for stroing options inside the poll struct
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, InitSpace)]
+pub struct Options {
+    #[max_len(64)]
+    pub description: String,
+    pub votes: u64,
+}
+
+//designing struct to store list of voters to prevent duplicate votes
+#[account]
+#[derive(InitSpace)]
+pub struct VoterRecord {
+    pub poll: Pubkey,     //Pubkey of the poll for which voter has voted
+    pub voter: Pubkey,    //Pubkey of the voter
+    pub option_index: u8, //Index of the option selected by voter
+}
+
 #[derive(Accounts)]
 pub struct Initialize {}
