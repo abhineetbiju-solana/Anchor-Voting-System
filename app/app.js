6baebdc1b25d727f17bdb0d48772db675ecfbd86
Abhineet Biju <abhineet.biju19@gmail.com>
2026-01-27 21:04:18 +0530

    fix(ui): run displayPolls() on startup before connecting the wallet

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import { showToast } from './utils/toast.js';
 import { fetchActivePolls, isWalletConnected, vote, getWalletPubKey, endPoll } from './utils/anchorClient.js';
 
+await displayPolls();
 
 // Handle Add Option button click
 document.getElementById('add-option').addEventListener('click', () => {
